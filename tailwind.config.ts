02e548b0b44de893df8fac7cedcd56addeec1afc
huy1231a <taolahuy2k0@gmail.com>
2024-01-19 19:54:32 +0700

    git add .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primaryColor: '#0067FF',
+        yellowColor: '#FEB60D',
+        purpleColor: '#9771FF',
+        irisBlueColor: '#01B5C5',
+        headingColor: '#181A1E',
+        textColor: '#4E545F',
+      },
+
+      boxShadow: {
+        panelShadow: 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
